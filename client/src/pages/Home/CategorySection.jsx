93a1e87b70b056231b2e9bc47e2df0cc4f9e2dab
Md Arwaz <mdarwaznew2017@gmail.com>
2025-11-01 13:08:12 +0530

    add navigate to product in category click

--- client/src/pages/Home/CategorySection.jsx
+++ client/src/pages/Home/CategorySection.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const CategorySection = () => {
   return (
     <>
@@ -5,108 +7,108 @@ const CategorySection = () => {
         <div className="container">
           <div className="row category_2_slider">
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/waterpurifier.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Water Purifier</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/watersoftners.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Water Softners</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/industrialstoragetanks.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Industrial / Storage Tanks</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/etp.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> E.T.P/S.T.P/WTP/Z.I.D</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/waterjonizers.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Water Jonizers</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/dispensor.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Water Cooler's Dispensor's</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/organicwaste.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Organic Waste Composting Machine</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/kitchen.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Kitchen/Home Appliance's</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/chimney.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Chimney</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/airpurifier.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Air Purifier/Air Cooler</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/sparesparts.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Spares Parts</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/chemicals.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Chemicals</h3>
-              </a>
+              </Link>
             </div>
             <div className="catsli wow fadeInUp">
-              <a href="#" className="category_item">
+              <Link to="/products" className="category_item">
                 <div className="img">
                   <img src="assets/maingraphics/cleaning.png" alt="Category" className="img-fluid w-100" />
                 </div>
                 <h3> Cleaning Essentials</h3>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
